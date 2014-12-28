7241cbab8d1e7891d669a22b19432cc4235db3d6
nverma1983 <narendra.verma14@gmail.com>
2014-12-28 14:01:49 +0530

    Added sample Android-PhoneGap-Angular app
    Added sample Android-PhoneGap-Angular app

---
+++ FirstHybridApp/app/src/main/assets/www/js/app.js
@@ -0,0 +1,4 @@
+var nvhybridandroidapp = angular.module("nvhybridandroidapp", []);
+nvhybridandroidapp.controller("MyController", function ($scope) {
+   $scope.message = "Welcome to first android app built using PhoneGap and AngularJS";
+});
